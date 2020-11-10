80862f6c3bab09037b1ec6e280f2b8bc6bfbe8c9
卿嘉俊 <47295240+cmgyqjj@users.noreply.github.com>
2020-11-10 10:16:57 +0800

    Update MsmSerciceImpl.java

--- guli_parent/service/service_msm/src/main/java/com/atguigu/msmservice/service/impl/MsmSerciceImpl.java
+++ guli_parent/service/service_msm/src/main/java/com/atguigu/msmservice/service/impl/MsmSerciceImpl.java
@@ -22,7 +22,7 @@ public class MsmSerciceImpl implements MsmService {
         if(StringUtils.isEmpty(phone)) return false;
 
         DefaultProfile profile =
-                DefaultProfile.getProfile("default", "LTAI4GGtub9xNwk8kW5RBQ18", "5pOe84GTzy0s5VI6tcsyiPAhGZoO0u");
+                DefaultProfile.getProfile("default", "LTAI4GGtub9xNwk8BQ18", "5pOe84GTzy0s5csyiPAhGZoO0u");
         IAcsClient client = new DefaultAcsClient(profile);
 
         //设置相关固定的参数
